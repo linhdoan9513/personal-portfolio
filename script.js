b315ed1fc6b695b11e8bf98984ffbaf2f66efe2d
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-10-16 14:39:20 -0400

    adding navbar

---
+++ script.js
@@ -0,0 +1,17 @@
+// Navigation bar
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+const mobileMenu = () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+};
+
+const closeMenu = () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+};
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
